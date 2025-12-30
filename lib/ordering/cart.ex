a99e1f3d0c830086e15c8863fa96a3e02ad1a405
Timofey Shufletyuk <qotofey@gmail.com>
2025-12-30 03:14:56 +0300

    Feat: Cart

--- lib/ordering/cart.ex
+++ lib/ordering/cart.ex
@@ -3,12 +3,28 @@ defmodule Ordering.Cart do
 
   def new(), do: %Ordering.Cart{}
 
+  def set_item(cart, item) do
+    new_items =
+      Map.put(
+        cart.items,
+        item.product_id,
+        item
+      )
+
+    %Ordering.Cart{cart | items: new_items}
+  end
+
   def items(cart) do
     cart.items
     |> Enum.map(fn {_, item} -> item end)
   end
 
-  def clear(cart) do
+  def clear_item(cart, product_id) do
+    new_items = Map.delete(cart.items, product_id)
+    %Ordering.Cart{cart | items: new_items}
+  end
+
+  def clear_all(cart) do
     %Ordering.Cart{cart | items: %{}}
   end
 end
