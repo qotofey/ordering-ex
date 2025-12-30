edce438f9796f6656d2ca8638d9e37e2d889c3e5
Timofey Shufletyuk <qotofey@gmail.com>
2025-12-30 03:14:56 +0300

    Feat: Cart

--- test/ordering/cart_test.exs
+++ test/ordering/cart_test.exs
@@ -13,5 +13,49 @@ defmodule Ordering.CartTest do
   end
 
   describe "set_item/2" do
+    test "add new product to empty cart" do
+      # Given
+      cart = Cart.new()
+      item = %{product_id: 1, qty: 3}
+
+      # When
+      updated_cart = Cart.set_item(cart, item)
+
+      # Then
+      assert %Cart{} = updated_cart
+      assert updated_cart.items[1] == item
+    end
+
+    test "update product in cart" do
+      # Given
+      cart = Cart.new()
+      item = %{product_id: 1, qty: 7}
+      cart = Cart.set_item(cart, item)
+      new_item = %{product_id: 1, qty: 2}
+
+      # When
+      updated_cart = Cart.set_item(cart, new_item)
+
+      # Then
+      assert %Cart{} = updated_cart
+      assert updated_cart.items[1] == new_item
+      assert Enum.count(updated_cart.items) == 1
+    end
+
+    test "add product to cart" do
+      # Given
+      cart = Cart.new()
+      item = %{product_id: 1, qty: 7}
+      cart = Cart.set_item(cart, item)
+      new_item = %{product_id: 3, qty: 2}
+
+      # When
+      updated_cart = Cart.set_item(cart, new_item)
+
+      # Then
+      assert %Cart{} = updated_cart
+      assert updated_cart.items[3] == new_item
+      assert Enum.count(updated_cart.items) == 2
+    end
   end
 end
