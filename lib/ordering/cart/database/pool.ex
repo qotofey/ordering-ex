355a5f733daaa999e85cbbe59bb38fbf7333d694
Timofey Shufletyuk <31373356+qotofey@users.noreply.github.com>
2026-01-06 00:00:19 +0300

    Feat: Database pool & workers (#6)

---
+++ lib/ordering/cart/database/pool.ex
@@ -0,0 +1,53 @@
+defmodule Ordering.Cart.Database.Pool do
+  use GenServer
+
+  @db_folder "./persist"
+  @workers 4
+
+  alias Ordering.Cart
+
+  def start do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def store(key, data) do
+    key
+    |> choose_worker()
+    |> Cart.Database.Worker.store(key, data)
+  end
+
+  def get(key) do
+    key
+    |> choose_worker()
+    |> Cart.Database.Worker.get(key)
+  end
+
+  def choose_worker(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  @impl GenServer
+  def init(_) do
+    File.mkdir_p!(@db_folder)
+
+    {:ok, start_workers()}
+  end
+
+  @impl GenServer
+  def handle_call({:get, key}, _from, workers) do
+    worker_key = :erlang.phash2(key, @workers)
+
+    {
+      :reply,
+      Map.get(workers, worker_key),
+      workers
+    }
+  end
+
+  defp start_workers() do
+    for index <- 1..3, into: %{} do
+      {:ok, pid} = Cart.Database.Worker.start(@db_folder)
+      {index - 1, pid}
+    end
+  end
+end
---
+++ lib/ordering/cart/database/worker.ex
@@ -0,0 +1,46 @@
+defmodule Ordering.Cart.Database.Worker do
+  use GenServer
+
+  def start(db_folder \\ "./persist") do
+    GenServer.start(__MODULE__, db_folder)
+  end
+
+  def store(worker_pid, key, data) do
+    GenServer.cast(worker_pid, {:store, key, data})
+  end
+
+  def get(worker_pid, key) do
+    GenServer.call(worker_pid, {:get, key})
+  end
+
+  @impl GenServer
+  def init(db_folder) do
+    File.mkdir_p!(db_folder)
+
+    {:ok, db_folder}
+  end
+
+  @impl GenServer
+  def handle_cast({:store, key, data}, db_folder) do
+    db_folder
+    |> file_name(key)
+    |> File.write!(:erlang.term_to_binary(data))
+
+    {:noreply, db_folder}
+  end
+
+  @impl GenServer
+  def handle_call({:get, key}, _from, db_folder) do
+    data =
+      case File.read(file_name(db_folder, key)) do
+        {:ok, content} -> :erlang.binary_to_term(content)
+        _ -> nil
+      end
+
+    {:reply, data, db_folder}
+  end
+
+  defp file_name(db_folder, key) do
+    Path.join(db_folder, to_string(key))
+  end
+end
